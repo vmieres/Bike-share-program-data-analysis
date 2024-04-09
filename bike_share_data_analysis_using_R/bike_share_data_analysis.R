2a48e188beab742fda48241ab94c4909dd4524cc
victormieres <mieresvictor01@gmail.com>
2024-04-09 18:23:56 -0400

    pushing notebook progress

--- bike_share_data_analysis_using_R/bike_share_data_analysis.R
+++ bike_share_data_analysis_using_R/bike_share_data_analysis.R
@@ -1,1 +1,22 @@
 install.packages(c('readr', 'ggplot2', 'tidyr'))
+# # # # # # # # # # # # # # # # # # # # # # # 
+# Install required packages
+# tidyverse for data import and wrangling
+# libridate for date functions
+# ggplot for visualization
+# # # # # # # # # # # # # # # # # # # # # # #  
+library(tidyverse)  #helps wrangle data
+library(lubridate)  #helps wrangle date attributes
+library(ggplot2)  #helps visualize data
+getwd() #displays your working directory
+setwd('/cloud/project/Resources') #sets your working directory to simplify calls to data)
+
+#=====================
+# STEP 1: COLLECT DATA
+#=====================
+# Upload Divvy datasets (csv files) here
+q1_2020 <- read_csv("Divvy_Trips_2019_Q1.csv")
+q2_2019 <- read_csv("Divvy_Trips_2019_Q2.csv")
+q3_2019 <- read_csv("Divvy_Trips_2019_Q3.csv")
+q4_2019 <- read_csv("Divvy_Trips_2019_Q4.csv")
+
